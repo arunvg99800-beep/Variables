91050957769fdb5a75826203db59220c635250e3
arunvg99800-beep <arunvg99800@gmail.com>
2025-10-08 02:03:58 +0530

    05

---
+++ src/Identifiers.java
@@ -0,0 +1,23 @@
+public class Identifiers
+{
+    public static void main(String[] args)
+    {
+       // int rollno 1 = 1; Space not allowed in between Identifiers
+       // System.out.println(rollno1);//
+
+        //int -roolnum = 2; [Special char's are not allowed expect $ and _]
+        //System.out.println(roolnum);//
+
+        //int 3roolnumb = 3; numbers cannot use as a first char
+        //System.out.println(3roolnumb);//
+
+        int $rool_4num = 4;
+        System.out.println($rool_4num);
+
+        //int abstract = 100; pre-defined keywords can't use
+       // System.out.println(abstract);//
+
+        int private1 = 5;
+        System.out.println(private1);
+    }
+}
