a89fe7143864b3ccd3a3fc3a4ff7f78c265b7ecb
arunvg99800-beep <arunvg99800@gmail.com>
2025-10-08 02:16:42 +0530

    06

---
+++ src/Identifiers2.java
@@ -0,0 +1,33 @@
+public class Identifiers2
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
+   class test// lower cases are not allowed to create class
+   {
+
+   }
+    class Test// Always start with Upper case
+    {
+
+    }
+    class 1Test // First char should not be number
+   {
+
+   }
+    class test java  // Space not allowed
+    {
+
+    }
+
+    class %Test// Special chars are not allowed expect $ and _
+{
+
+}
+  class _King
+  {
+
+
+  }
